3ef0b8cc97f1d1098a1f4ea09604bced999384f2
Makar Mikhalchenko <76819996+MakarMS@users.noreply.github.com>
2025-08-06 00:10:33 +0300

    refactor: Apply SOLID principles to notification system

--- src/main/java/com/makar/Main.java
+++ src/main/java/com/makar/Main.java
@@ -1,21 +1,31 @@
 package com.makar;
 
+class SendingException extends Exception {
+    public SendingException(String message) {
+        super(message);
+    }
+}
+
 interface INotifier {
+    void send(String message) throws SendingException;
+}
+
+interface IEmailConnection {
     void connect();
 
-    void send(String message);
+    void send(String message) throws IllegalArgumentException;
 
     void disconnect();
 }
 
-class EmailNotifier implements INotifier {
+class SMTPConnection implements IEmailConnection {
     public void connect() {
         System.out.println("Connecting to email server...");
     }
 
     public void send(String message) {
-        if (message.length() > 160) {
-            throw new IllegalArgumentException("Message too long!");
+        if (message.length() > 1000) {
+            throw new IllegalArgumentException("Message too large");
         }
         System.out.println("Sending EMAIL: " + message);
     }
@@ -25,38 +35,48 @@ class EmailNotifier implements INotifier {
     }
 }
 
-class WebhookNotifier implements INotifier {
-    public void connect() {
-        throw new UnsupportedOperationException("WebhookNotifier does not support connect()");
+class EmailNotifier implements INotifier {
+
+    private final IEmailConnection connection;
+
+    public EmailNotifier(IEmailConnection connection) {
+        this.connection = connection;
     }
 
+    public void send(String message) throws SendingException {
+        this.connection.connect();
+        try {
+            this.connection.send(message);
+        } catch (IllegalArgumentException e) {
+            throw new SendingException("Message not sent, because: " + e.getMessage());
+        } finally {
+            this.connection.disconnect();
+        }
+    }
+}
+
+class WebhookNotifier implements INotifier {
     public void send(String message) {
         System.out.println("Sending webhook with message: " + message);
-    }
-
-    public void disconnect() {
-        throw new UnsupportedOperationException("WebhookNotifier does not support disconnect()");
     }
 }
 
 class NotificationSender {
-    public void sendNotification(INotifier notifier, String message) {
-        if (notifier instanceof EmailNotifier) {
-            notifier.connect();
-            notifier.send(message);
-            notifier.disconnect();
-        } else if (notifier instanceof WebhookNotifier) {
-            notifier.send(message);
-        } else {
-            System.out.println("Unsupported notification type");
-        }
+    public void sendNotification(INotifier notifier, String message) throws SendingException {
+        notifier.send(message);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         NotificationSender sender = new NotificationSender();
-        sender.sendNotification(new EmailNotifier(), "Welcome!");
-        sender.sendNotification(new WebhookNotifier(), "Webhook message here.");
+
+        try {
+            sender.sendNotification(new EmailNotifier(new SMTPConnection()), "Welcome!");
+            sender.sendNotification(new WebhookNotifier(), "Webhook message here.");
+            sender.sendNotification(new EmailNotifier(new SMTPConnection()), "A".repeat(1001));
+        } catch (SendingException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
